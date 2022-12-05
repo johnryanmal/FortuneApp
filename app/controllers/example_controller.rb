756365cda1f49cd0177328180b67f5926d95a476
“johnryanmal” <johnryanmal@gmail.com>
2022-12-05 09:42:08 -0800

    make feature fortune

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,2 +1,8 @@
 class ExampleController < ApplicationController
+  def read_fortune
+    fortunes = ['Great fortune', 'Good fortune', 'Ominous fortune']
+    render json: {
+      fortune: fortunes[rand(fortunes.length)]
+    }
+  end
 end
